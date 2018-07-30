df9b9b5cb5e3c7079ac169ea309409214f21e766
kallip <kallinpettit@gmail.com>
2018-07-30 15:03:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,17 @@
 # dog.rb
-class Dog
+class Dog  
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+  def name
+    @name 
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end 
+end
